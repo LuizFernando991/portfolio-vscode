58683c1af18ad5be0d8c203b6e1d4b4d098b914d
LuizFernando991 <lfernando.r991@gmail.com>
2025-01-29 16:02:36 -0300

    feat: translations setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client'
 import { TabProvider } from './contexts/tabContext.jsx'
 import { MenuProvider } from './contexts/menuContext.jsx'
 import { DirectoryProvider } from './contexts/directoryContext.jsx'
-import './index.css'
 import App from './App.jsx'
+import './translations/i18n'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
---
+++ src/translations/i18n.jsx
@@ -0,0 +1,28 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import enTranslation from './locales/en.json'
+import ptTranslation from './locales/pt.json'
+import LanguageDetector from 'i18next-browser-languagedetector'
+
+const resources = {
+  en: { translation: enTranslation },
+  pt: { translation: ptTranslation }
+}
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    debug: true,
+    interpolation: {
+      escapeValue: false
+    },
+    detection: {
+      order: ['navigator', 'localStorage', 'sessionStorage', 'cookie'],
+      caches: ['localStorage', 'cookie']
+    }
+  })
+
+export default i18n
