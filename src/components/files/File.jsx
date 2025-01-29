5fa9ef1d0d07951d555cf59e0e1162348f315d3d
LuizFernando991 <lfernando.r991@gmail.com>
2025-01-29 13:45:28 -0300

    style: change FileIcon styles

--- src/components/files/File.jsx
+++ src/components/files/File.jsx
@@ -2,7 +2,7 @@ import { FileIcon } from './FileIcon'
 
 export function File({ name, extension }) {
   return (
-    <div className="flex items-center justify-center text-sm">
+    <div className="flex items-center text-sm w-full">
       <FileIcon fileType={extension} />
       <span>
         {name}.{extension}
--- src/components/files/FileIcon.jsx
+++ src/components/files/FileIcon.jsx
@@ -1,5 +1,12 @@
 import { fileIcons } from '../../assets/icons/files_types/fileIcons'
 
 export function FileIcon({ fileType = 'defaultIcon' }) {
-  return <img src={fileIcons[fileType]} className="w-6 h-6 mr-2" alt="" />
+  return (
+    <img
+      draggable={false}
+      src={fileIcons[fileType]}
+      className="w-6 h-6 mr-2"
+      alt={fileType}
+    />
+  )
 }
