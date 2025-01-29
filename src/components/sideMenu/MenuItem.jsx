2f9f7b37860e133029dfccb560f911993d32d46b
LuizFernando991 <lfernando.r991@gmail.com>
2025-01-29 13:46:20 -0300

    style: remove styles consts

--- src/components/sideMenu/MenuItem.jsx
+++ src/components/sideMenu/MenuItem.jsx
@@ -1,24 +1,23 @@
 import classNames from 'classnames'
 
 export function MenuItem({ selected, Icon, onClick }) {
-  const defaultContainerClasses =
-    'flex items-center border-l-2 border-dark-600 p-2 cursor-pointer group'
-  const selectedContainerClasses = {
-    'border-l-pink': selected,
-    'bg-dark-400': selected,
-    'bg-dark-600': !selected
-  }
-  const iconDefualtClases = 'w-7 h-8 group-hover:text-white'
-  const selectedIconClasses = {
-    'text-white-50a': !selected
-  }
-
   return (
     <button
       onClick={onClick}
-      className={classNames(defaultContainerClasses, selectedContainerClasses)}
+      className={classNames(
+        'flex items-center border-l-2 border-dark-600 p-2 cursor-pointer group',
+        {
+          'border-l-pink': selected,
+          'bg-dark-400': selected,
+          'bg-dark-600': !selected
+        }
+      )}
     >
-      <Icon className={classNames(iconDefualtClases, selectedIconClasses)} />
+      <Icon
+        className={classNames('w-7 h-8 group-hover:text-white', {
+          'text-white-50a': !selected
+        })}
+      />
     </button>
   )
 }
