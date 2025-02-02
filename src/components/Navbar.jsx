2c8e096e66f9d12a2e928095a84f6c74a83d37bd
LuizFernando991 <lfernando.r991@gmail.com>
2025-02-01 21:29:00 -0300

    style: change NavBar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,8 @@ import { TabBar } from './tabs/TabBar'
 
 export function Navbar() {
   return (
-    <div className="ml-1 md:ml-0 md:bg-inherit bg-dark-600">
-      <div className="flex">
-        <TabBar />
-      </div>
+    <div className="bg-dark-600 flex">
+      <TabBar />
     </div>
   )
 }
