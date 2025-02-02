9279517760b13dce723abef0828dbb685bc76563
LuizFernando991 <lfernando.r991@gmail.com>
2025-02-01 21:29:26 -0300

    chore: implements minimap in About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,7 @@
+import { useRef } from 'react'
 import { useTranslation } from 'react-i18next'
 import Lottie from 'lottie-react'
+import { Minimap } from './Minimap'
 import { Header } from './Header'
 import { BreakLine } from './BreakLine'
 import { Section } from './Section'
@@ -13,61 +15,74 @@ import enResume from '../assets/resume/ResumeLuiz.pdf'
 
 export function About() {
   const { t, i18n } = useTranslation()
+  const containerRef = useRef(null)
 
   const resume = i18n.language === 'pt' ? ptResume : enResume
 
-  return (
-    <div className="relative pt-4 pl-2 h-full flex">
-      <div className="grow max-w-[1200px] z-10">
-        <Section>
-          <Header
-            headingLevel="1"
-            text={t('about.title')}
-            className="text-3xl md:text-5xl"
-          />
-          <BreakLine />
-          <Text className="mb-10">
-            {t('about.p1')}
-            <TabLink
-              className="font-bold text-yellow cursor-pointer"
-              tab={tabsContent.projects}
-            >
-              {t('tabs.Projects')}.js
-            </TabLink>
-            .
-          </Text>
-          <Text className="mb-10">
-            {t('about.p2')}
-            <TabLink
-              className="font-bold text-yellow cursor-pointer"
-              tab={tabsContent.contact}
-            >
-              {t('tabs.Contact')}.md
-            </TabLink>
-            .
-          </Text>
+  const component = (animate = false) => (
+    <div className="relative grow max-w-[1200px] z-10 pt-2">
+      <Section>
+        <Header
+          headingLevel="1"
+          text={t('about.title')}
+          className="text-3xl md:text-5xl"
+        />
+        <BreakLine />
+        <Text className="mb-10">
+          {t('about.p1')}
+          <TabLink
+            className="font-bold text-yellow cursor-pointer"
+            tab={tabsContent.projects}
+          >
+            {t('tabs.Projects')}.js
+          </TabLink>
+          .
+        </Text>
+        <Text className="mb-10">
+          {t('about.p2')}
+          <TabLink
+            className="font-bold text-yellow cursor-pointer"
+            tab={tabsContent.contact}
+          >
+            {t('tabs.Contact')}.md
+          </TabLink>
+          .
+        </Text>
 
-          <Text>
-            {t('about.p3')}
+        <Text>
+          {t('about.p3')}
+          {animate && (
             <span className="animate-blink text-white-75a text-[1.1rem]">
               |
             </span>
-          </Text>
-        </Section>
-        <BreakLine />
-        <Section>
-          <Header
-            headingLevel="2"
-            text={t('about.title-2')}
-            className="text-xl md:text-3xl"
-          />
-          <Text>{t('about.p4')}</Text>
-          <MarkdownButton as="a" href={resume} target="_blank" rel="noreferrer">
-            {t('about.download-button')}
-          </MarkdownButton>
-        </Section>
+          )}
+        </Text>
+      </Section>
+      <BreakLine />
+      <Section>
+        <Header
+          headingLevel="2"
+          text={t('about.title-2')}
+          className="text-xl md:text-3xl"
+        />
+        <Text>{t('about.p4')}</Text>
+        <MarkdownButton as="a" href={resume} target="_blank" rel="noreferrer">
+          {t('about.download-button')}
+        </MarkdownButton>
+      </Section>
+    </div>
+  )
+
+  return (
+    <div
+      ref={containerRef}
+      className="px-2 md:pl-10 md:px-0 pb-10 flex w-full justify-between gap-1 overflow-y-auto overflow-hidden font-mono"
+    >
+      {component(true)}
+      <div className="w-52 z-10 sticky top-0 hidden lg:block">
+        <Minimap of={component()} scrollContainerRef={containerRef} />
       </div>
-      <div className="absolute w-full h-full hidden lg:block opacity-[1%]  ">
+      <div className="fixed top-0 left-0 right-0 w-full -z-100 h-full hidden lg:block opacity-[1%]">
         <Lottie animationData={animationData} loop={true} />
       </div>
     </div>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,31 +1,14 @@
-import { useEffect, useRef } from 'react'
 import { Navbar } from '../components/Navbar'
 import { useTabContext } from '../hooks/useTabContext'
 
 export function Dashboard() {
   const tabs = useTabContext()
-  const tabRef = useRef(null)
-
-  useEffect(() => {
-    if (tabRef.current) {
-      tabRef.current.scrollTo({ top: 20, behaviour: 'smooth' })
-    }
-  }, [tabs])
 
   return (
     <div className="font-sans relative w-full h-full flex overflow-hidden">
-      <div className="flex-1 flex flex-col w-full overflow-hidden">
+      <div className="flex-1 flex flex-col w-full overflow-hidden gap-2">
         <Navbar />
-        {tabs.current ? (
-          <div
-            ref={tabRef}
-            className="h-full font-mono pt-2 pb-[100px] px-2 overflow-y-auto flex-1"
-          >
-            <div className="w-full min-h-full">{tabs.current.component}</div>
-          </div>
-        ) : (
-          <div>Não tem tab</div>
-        )}
+        {tabs.current ? tabs.current.component : <div>Não tem tab</div>}
       </div>
     </div>
   )
