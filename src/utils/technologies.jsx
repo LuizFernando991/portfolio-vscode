20011a3ff062dfa770a6ccf4cdbd9b546350e0cb
LuizFernando991 <lfernando.r991@gmail.com>
2025-02-26 11:52:23 -0300

    feat: create my infos technologies arrays

---
+++ src/utils/technologies.jsx
@@ -0,0 +1,128 @@
+import {
+  SiJavascript,
+  SiNextdotjs,
+  SiTypescript,
+  SiSocketdotio,
+  SiTailwindcss,
+  SiMongodb,
+  SiReact,
+  SiExpress,
+  SiPrisma,
+  SiNestjs,
+  SiPostgresql,
+  SiGraphql,
+  SiAmazonwebservices,
+  SiDocker,
+  SiGit,
+  SiMysql,
+  SiRabbitmq,
+  SiRust,
+  SiBun,
+  SiTerraform,
+  SiDrizzle
+} from 'react-icons/si'
+import { FaNode } from 'react-icons/fa'
+import { FaGolang } from 'react-icons/fa6'
+import { DiRedis } from 'react-icons/di'
+
+export const technologies = [
+  {
+    name: 'TypeScript',
+    icon: SiTypescript
+  },
+  {
+    name: 'JavaScript',
+    icon: SiJavascript
+  },
+  {
+    name: 'Node.js',
+    icon: FaNode
+  },
+  {
+    name: 'Golang',
+    icon: FaGolang
+  },
+  {
+    name: 'Nest.js',
+    icon: SiNestjs
+  },
+  {
+    name: 'Next.js',
+    icon: SiNextdotjs
+  },
+  {
+    name: 'React.js',
+    icon: SiReact
+  },
+  {
+    name: 'Express',
+    icon: SiExpress
+  },
+  {
+    name: 'GraphQL',
+    icon: SiGraphql
+  },
+  {
+    name: 'Socket.io',
+    icon: SiSocketdotio
+  },
+  {
+    name: 'MongoDB',
+    icon: SiMongodb
+  },
+  {
+    name: 'PostgresSQL',
+    icon: SiPostgresql
+  },
+  {
+    name: 'MySQL',
+    icon: SiMysql
+  },
+  {
+    name: 'Prisma',
+    icon: SiPrisma
+  },
+  {
+    name: 'Tailwind',
+    icon: SiTailwindcss
+  },
+  {
+    name: 'AWS',
+    icon: SiAmazonwebservices
+  },
+  {
+    name: 'Docker',
+    icon: SiDocker
+  },
+  {
+    name: 'RabbitMQ',
+    icon: SiRabbitmq
+  },
+  {
+    name: 'Redis',
+    icon: DiRedis
+  },
+  {
+    name: 'Git',
+    icon: SiGit
+  }
+]
+
+export const actuallyStuding = [
+  {
+    name: 'Rust',
+    icon: SiRust
+  },
+  {
+    name: 'Bun',
+    icon: SiBun
+  },
+  {
+    name: 'Terraform',
+    icon: SiTerraform
+  },
+  {
+    name: 'Drizzle',
+    icon: SiDrizzle
+  }
+]
