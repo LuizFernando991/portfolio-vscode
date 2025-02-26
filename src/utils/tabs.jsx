e6d69fd1ba00204c4e539363a4983412e117d7c1
LuizFernando991 <lfernando.r991@gmail.com>
2025-02-26 11:53:49 -0300

    chore: add Technology tab

--- src/utils/tabs.jsx
+++ src/utils/tabs.jsx
@@ -1,12 +1,18 @@
 import { About } from '../components/About'
 import { Contact } from '../components/Contact'
 import { Home } from '../components/Home'
+import { Technologies } from '../components/technologies/Technologies'
 
 export const tabsContent = {
   home: { name: 'Home', type: 'file', extension: 'js', component: <Home /> },
   about: { name: 'About', type: 'file', extension: 'js', component: <About /> },
   projects: { name: 'Projects', type: 'file', extension: 'js' },
-  technologies: { name: 'Technologies', type: 'file', extension: 'js' },
+  technologies: {
+    name: 'Technologies',
+    type: 'file',
+    extension: 'js',
+    component: <Technologies />
+  },
   index: { name: 'index', type: 'file', extension: 'js' },
   contact: {
     name: 'Contact',
