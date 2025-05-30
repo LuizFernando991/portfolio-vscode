7b61a0bf50daabe657e1075f79e996c764c073e1
LuizFernando991 <lfernando.r991@gmail.com>
2025-05-30 17:53:46 -0300

    fix: heigth on mobile devices

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { FinalLine } from './components/FinalLine'
 function App() {
   const menu = useMenuContext()
   return (
-    <div className="font-sans h-screen flex flex-col overflow-hidden">
+    <div className="font-sans h-[100dvh] flex flex-col overflow-hidden">
       <div className="flex-1 flex w-full overflow-hidden">
         <SideMenu />
         {menu.currentPage.component}
